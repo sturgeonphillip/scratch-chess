381bf07869363a258aabfc1edd17162ebef7019d
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2022-03-22 16:11:51 -0700

    delete old unnecessary files

--- src/images/vecteezy_set-of-chess-perfect-for-design-project_2128019/chess-vecteezy-license.tsx
+++
@@ -1,9 +0,0 @@
-export default function vecteezyLicense() {
-  return (
-    <>
-      <a href="https://www.vecteezy.com/free-vector/king-chess-piece">
-        King Chess Piece Vectors by Vecteezy
-      </a>
-    </>
-  );
-}
